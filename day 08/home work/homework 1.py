d80e8d57df3e74328d1947f7e58857cd7b0fa4df
lukapapshvili <lukapapashvili521@gmail.com>
2024-07-31 13:18:40 -0700

    commit

--- day 08/home work/homework 1.py
+++ day 08/home work/homework 1.py
@@ -1,12 +1,19 @@
-required_weight = 50
-required_height = 170
-user_weight = int (input("enter your weight: "))
-user_height = int (input("enter your height: "))
-print(required_weight > user_weight and required_height > user_height)
-print(required_weight < user_weight and required_height > user_height)
-print(required_weight > user_weight and required_height < user_height)
-print(required_weight < user_weight and required_height < user_height)
-print(required_weight <= user_weight and required_height <= user_height)
-print(required_weight >= user_weight and required_height <= user_height)
-print(required_weight <= user_weight and required_height >= user_height)
-print(required_weight >= user_weight and required_height >= user_height)
+# num = 10
+# for i in range (10):
+#    print(num)
+
+# num = 20
+# num1 = 30
+# for i in range ((num  + 10)+ num1 +10):
+#     print(num + num1)
+
+# num1 = 213
+# num2 = 421
+# for i in range ((num1 + 10)* num2 + 10):
+#     print(num1 * num2)
+
+
+# num1 = 512
+# num2 = 13
+# for i in range ((num1 + 10)- num2 + 10):
+#     print(num1 - num2)
